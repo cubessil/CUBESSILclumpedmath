5a5f453320e0e1571e77dc8813bb346c4a8c6009
cubessil <39316400+cubessil@users.noreply.github.com>
2019-05-12 10:15:44 -0600

    Update getdatafromAnalysis.R
    Fixe

--- R/getdatafromAnalysis.R
+++ R/getdatafromAnalysis.R
@@ -1,10 +1,10 @@
 #function for fixing
 #fix any errors in sample names, and/or add comments about analysis to comment column here; added to combined_data_group because this is the dataset that gets written to the flatlist.csv file
 
-getmedadatafromAnalysis <- function(dataframe, lowerboundAnalysis, uperboundAnalysis, getmedadatafromAnalysis){
-  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= uperboundAnalysis, "Identifier 1"] <- first(dataframe[ floor(dataframe$Analysis) == getmedadatafromAnalysis, "Identifier 1"])[1]
-  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= uperboundAnalysis, "Identifier 2"] <- first(dataframe[ floor(dataframe$Analysis) == getmedadatafromAnalysis, "Identifier 2"])[1]
-  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= uperboundAnalysis, "Preparation"] <- first(dataframe[ floor(dataframe$Analysis) == getmedadatafromAnalysis, "Preparation"])[1]
-  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= uperboundAnalysis, "type"] <- first(dataframe[ floor(dataframe$Analysis) == getmedadatafromAnalysis, "type"])[1]
+getmetadatafromAnalysis <- function(dataframe, lowerboundAnalysis, upperboundAnalysis, getmetadatafromAnalysis){
+  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= upperboundAnalysis, "Identifier 1"] <- first(dataframe[ floor(dataframe$Analysis) == getmetadatafromAnalysis, "Identifier 1"])[1]
+  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= upperboundAnalysis, "Identifier 2"] <- first(dataframe[ floor(dataframe$Analysis) == getmetadatafromAnalysis, "Identifier 2"])[1]
+  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= upperboundAnalysis, "Preparation"] <- first(dataframe[ floor(dataframe$Analysis) == getmetadatafromAnalysis, "Preparation"])[1]
+  dataframe[dataframe$Analysis >= lowerboundAnalysis & dataframe$Analysis <= upperboundAnalysis, "type"] <- first(dataframe[ floor(dataframe$Analysis) == getmetadatafromAnalysis, "type"])[1]
   return(dataframe)
 }
