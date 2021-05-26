30444f8d7ba65b77435b5569788cdd3010f7561b
cubessil <39316400+cubessil@users.noreply.github.com>
2021-05-26 15:39:37 -0600

    Updated documentation tags

--- R/convert_ARF.D47_to_ARF.Anderson.temp.R
+++ R/convert_ARF.D47_to_ARF.Anderson.temp.R
@@ -1,5 +1,10 @@
-#' Calculate clumped isotope temperatures using the composite calibration from Anderson et al., 2021
-#' @ D47_90 indicates that the clumped values to use are those in 90degC space, ie not acid digestion corrected
+#' Anderson Clumped Isotope Calibration
+#' 
+#' Calculates clumped isotope temperatures using the composite calibration from Anderson et al., 2021
+#'
+#' @param D47_90 numeric vector of D47 values to use to calculate temperatures. This calibrations needs D47 values that are 90degC values, ie not acid digestion corrected
+#'
+#' @export
 
 convert_ARF.D47_to_ARF.Anderson.temp <- function(D47_90) {
   round(((0.0391*10^6)/(D47 - 0.154))^0.5 - 273.15, 1)
