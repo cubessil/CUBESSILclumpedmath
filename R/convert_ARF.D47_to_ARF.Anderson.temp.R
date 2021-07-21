a927880442c1426d0a451e9dd50326982195827a
cubessil <39316400+cubessil@users.noreply.github.com>
2021-07-21 07:18:25 -0600

    updated variable name

--- R/convert_ARF.D47_to_ARF.Anderson.temp.R
+++ R/convert_ARF.D47_to_ARF.Anderson.temp.R
@@ -7,5 +7,5 @@
 #' @export
 
 convert_ARF.D47_to_ARF.Anderson.temp <- function(D47_90) {
-  round(((0.0391*10^6)/(D47 - 0.154))^0.5 - 273.15, 1)
+  round(((0.0391*10^6)/(D47_90 - 0.154))^0.5 - 273.15, 1)
 }
